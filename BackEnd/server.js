72cd486ae64fb67b89502c1425f6f5bd024f498f
facundodev7 <facundoemanuel73@gmail.com>
2026-05-29 19:48:03 -0300

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -13,7 +13,7 @@ app.use('/usuarios', routerUsuarios)
 
 app.get('/', (req,res)=>{
     res.status(200)
-    res.send(`<h1>El servidor esta funcionando, por ahora</h1>`)
+    res.send(`<h1 style="color: green">El servidor esta funcionando, por ahora</h1>`)
 })
 
 app.listen(PORT, HOSTNAME, (req, res)=>{
